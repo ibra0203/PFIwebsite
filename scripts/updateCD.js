36c5f11a3fea79a02b11eab2a8502a6aeb70f9e3
mody1710 <ibra0203@algonquinlive.com>
2018-01-20 04:23:34 -0400

    Initial commit

---
+++ scripts/updateCD.js
@@ -0,0 +1,38 @@
+var countdownHandler = (function(){
+    var initDone=false;
+    var countDownDate = new Date("May 5, 2018 15:37:25").getTime();
+    
+    function _init()
+    {
+        _setCountdownDate();
+     setInterval(_setCountdownDate, 1000);   
+    }
+    function _setCountdownDate()
+    {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        $("#CD-days").text(days);
+        $("#CD-hours").text(hours);
+        $("#CD-minutes").text(minutes);
+        $("#CD-seconds").text(seconds);
+        
+    }
+    
+    return{
+        init:function(){
+            if(!initDone)
+                {
+                    _init();
+                    initDone=true;
+                }
+        }
+    };
+}()
+);
+
+countdownHandler.init();
